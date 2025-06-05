26298c0cca2574d740f4cdce98b6eb9e77f762e8
Encerel <roman.khrechko@gmail.com>
2025-06-05 19:02:58 +0300

    Image not loading fix

--- blogicum/blogicum/settings.py
+++ blogicum/blogicum/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-a)kcx1(#@y%po(%d*3q#8q=@zdya+k-8mv@3a_h(pfqiaf7pi5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = []
 
